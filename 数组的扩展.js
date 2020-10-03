80c42827fd60fdc18f671e088783c384d9640e51
duzhuo <1638388435@qq.com>
2020-10-03 16:46:42 +0800

    add 扩展运算符3应用

--- 数组的扩展.js
+++ 数组的扩展.js
@@ -52,5 +52,69 @@
 //ES6
 // arr1.push(...arr2);
 // console.log(arr1);
-console.log(new(Date.bind.apply(Date, [null, 2015, 1, 1])));
-console.log(new Date(...[2015, 1, 1]));
+// console.log(new(Date.bind.apply(Date, [null, 2015, 1, 1])));
+// console.log(new Date(...[2015, 1, 1]));
+
+//扩展运算符的应用
+//（1）复制数组
+// const a1 = [1, 2];
+// const a2 = a1;
+// a2[0] = 2;
+// console.log(a1);//[ 2, 2 ]
+// console.log(a2);//[ 2, 2 ]
+//实现一种浅拷贝
+// const a1 = [1, 2];
+// const a2 = a1.concat();
+// a2[0] = 2;
+// console.log(a1);//[ 1, 2 ]
+// console.log(a2);//[ 2, 2 ]
+//ES5写法，实现一种深拷贝
+// const a1 = [1, 2];
+// //const a2 = [...a1];
+// const [...a2] = a1;//效果相同
+// a2[0] = 2;
+// console.log(a1);//[ 1, 2 ]
+// console.log(a2);//[ 2, 2 ]
+//实现一种深拷贝
+//（2）合并数组
+// const arr1 = ['a', 'b'];
+// const arr2 = ['c'];
+// const arr3 = ['d', 'e'];
+// console.log(arr1.concat(arr2, arr3));//ES5的合并数组
+// console.log([...arr1, ...arr2, ...arr3]);//ES6的合并数组
+// const a1 = [{foo:1}];
+// const a2 = [{bar:2}];
+// const a3 = a1.concat(a2);
+// console.log(a3);//[ { foo: 1 }, { bar: 2 } ]
+// const a4 = [...a1, ...a2];
+// console.log(a4);
+// console.log(a3[0] === a1[0]);//true
+// console.log(a4[0] === a1[0]);//true
+//实现一种浅拷贝
+//（3）与解构赋值结合
+//伪代码
+// var list = [0, 1, 2, 3]
+// var a = list[0]
+// var rest = list.slice(1);//ES5
+// var a, rest=[];
+// [a, ...rest] = list;//ES6
+// console.log(a);
+// console.log(rest);
+// var a;
+// var rest = [1, 2, 3];
+// var list = [0, 1, 2, 3];
+// [a, ...rest] = list;
+// console.log(a);
+// console.log(rest);
+//other
+// const [first, ...rest] = [1, 2, 3, 4 ,5];
+// console.log(first);
+// console.log(rest);
+// const [first, ...rest] = [];
+// console.log(first);//undefined
+// console.log(rest);//[]
+// const [first, ...rest] = ["foo"];
+// console.log(first);//foo
+// console.log(rest);//[]
+//数组赋值，只能扩展运算符放在参数最后一位
+// const [...a, b] = [1, 2, 3];//SyntaxError: Rest element must be last element
